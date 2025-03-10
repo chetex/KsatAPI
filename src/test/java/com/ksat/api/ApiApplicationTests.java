2a625d416681818c517c9e05d98f5f12fb89a0d9
chetex <chetex69@gmail.com>
2025-03-10 20:23:25 +0100

    Initial REST version

---
+++ src/test/java/com/ksat/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksat.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
